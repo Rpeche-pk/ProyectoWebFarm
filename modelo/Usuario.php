30ca65e0749a38b0488d9abdedcf93ea5a9c6dc9
Rpeche-pk <pecheaparcana1998@gmail.com>
2021-08-30 11:54:36 -0500

    inicio del proyecto

---
+++ modelo/Usuario.php
@@ -0,0 +1,80 @@
+<?php
+    include_once "Conexion.php";
+
+    class Usuario{
+        var $objetos;
+        public function __construct()
+        {
+            $db= new Conexion();
+            $this->acceso= $db->pdo;
+        }
+        
+        function Loguearse($dni, $pass){
+            $sql="select * from usuario inner join tipo_us on us_tipo=id_tipo_us where dni_us=:dni and contrasena_us=:pass";
+            $query= $this->acceso->prepare($sql);
+            $query->execute(array(':dni'=>$dni, ':pass'=>$pass));
+            $this->objetos=$query->fetchall();
+            return $this->objetos;
+        }
+
+        function obtener_datos($id){
+            $sql="select * from usuario join tipo_us on us_tipo=id_tipo_us and id_usuario=:id";
+            $query= $this->acceso->prepare($sql);
+            $query->execute(array(':id'=>$id));
+            $this->objetos=$query->fetchall();
+            return $this->objetos;
+        }
+
+        function editar($id_usuario,$telefono,$residencia,$correo,$sexo,$adicional){
+            $sql="update usuario set telefono_us=:telefono, residencia_us=:residencia, correo_us=:correo, sexo_us=:sexo, adicional_us=:adicional where id_usuario=:id";
+            $query= $this->acceso->prepare($sql);
+            $query->execute(array(':id'=>$id_usuario, ':telefono'=>$telefono, ':residencia'=>$residencia, ':correo'=>$correo, ':sexo'=>$sexo, ':adicional'=>$adicional));                        
+        }
+
+        function cambiar_contra($id_usuario,$oldpass,$newpass){
+            $sql="SELECT * FROM usuario WHERE id_usuario=:id and contrasena_us=:oldpass";
+            $query= $this->acceso->prepare($sql);
+            $query->execute(array(':id'=>$id_usuario, ':oldpass'=>$oldpass));
+            $this->objetos = $query->fetchall();
+            if(!empty($this->objetos)){
+                $sql="UPDATE usuario SET contrasena_us=:newpass where id_usuario=:id";
+                $query=$this->acceso->prepare($sql);
+                $query->execute(array(':id'=>$id_usuario, ':newpass'=>$newpass));
+                echo 'update';
+            }
+            else{
+                echo 'noupdate';
+            }
+        }
+
+        function cambiar_photo($id_usuario,$nombre){
+            $sql="SELECT avatar FROM usuario WHERE id_usuario=:id";
+            $query= $this->acceso->prepare($sql);
+            $query->execute(array(':id'=>$id_usuario));
+            $this->objetos = $query->fetchall();
+            
+                $sql="UPDATE usuario SET avatar=:nombre where id_usuario=:id";
+                $query=$this->acceso->prepare($sql);
+                $query->execute(array(':id'=>$id_usuario, ':nombre'=>$nombre));     
+            return $this->objetos;              
+        }
+
+        function buscar(){
+            if(!empty($_POST['consulta'])){
+                $consulta=$_POST['consulta'];
+                $sql = "SELECT * FROM usuario join tipo_us on us_tipo=id_tipo_us where nombre_us LIKE :consulta";
+                $query = $this->acceso->prepare($sql);
+                $query->execute(array(':consulta'=>"%$consulta%"));
+                $this->objetos=$query->fetchall();
+                return $this->objetos;
+            }
+            else{                
+                $sql = "SELECT * FROM usuario join tipo_us on us_tipo=id_tipo_us where nombre_us NOT LIKE '' ORDER BY id_usuario LIMIT 25";
+                $query = $this->acceso->prepare($sql);
+                $query->execute();
+                $this->objetos=$query->fetchall();
+                return $this->objetos;
+            }
+        }
+    }
+?>
